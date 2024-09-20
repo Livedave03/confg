2f52db564b3de87ed129ca4e7fc8522bb35b3139
Livedave03 <lopelquelop@gmail.com>
2024-09-20 12:21:06 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const botToken = '7276471564:AAG3Komry3B-FtFHigG7xVVclDSFeHKjaXA'; // Reemplaza con tu bot token
+const chatId = '-4239800341'; // Reemplaza con tu chat ID
+
+export { botToken, chatId };
